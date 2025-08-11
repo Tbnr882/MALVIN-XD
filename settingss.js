e31189ca81a844f388f230dd5223d0e98505559e
Tbnr882 <faezakanni@icloud.com>
2025-08-11 20:55:10 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // malvin~ouUgAaDJ#7e9P2r3hDrx2WfvOrvAKwHXK0aRulCI-0_pLse1Uu68
+  SESSION_ID: "",  // "malvin~ouUgAaDJ#7e9P2r3hDrx2WfvOrvAKwHXK0aRulCI-0_pLse1Uu68"
   
-  OWNER_NUMBER: "263714757857", // 2347063122427
+  OWNER_NUMBER: "263714757857", // "2347063122427"
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: ".", // prefix "."
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
